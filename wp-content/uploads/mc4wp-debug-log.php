be0614e98e10acf995b9fde6d5967f0248b50e7b
HelenaJovcic <hele.hele.94@gmail.com>
2017-07-08 11:32:57 +0000

    First commit

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,6 @@
+<?php exit; ?>
+[2017-06-29 07:31:08] WARNING: Form 62 > cmil************@gm***.com is already subscribed to the selected list(s)
+[2017-06-29 07:36:17] INFO: Form 62 > Successfully subscribed tzmi**@ho*****.rs
+[2017-06-29 08:49:13] INFO: Form 62 > Successfully updated tzmi**@ho*****.rs
+[2017-06-29 08:50:09] INFO: Form 62 > Successfully subscribed lazn*****@ho*****.com
+[2017-06-29 08:55:07] INFO: Form 62 > Successfully subscribed lazn*****@ho*****.com
